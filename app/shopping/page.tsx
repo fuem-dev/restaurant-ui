154d13fa121ce779bc918246bf878405080eec9a
fuem-dev <60617260+fuem-dev@users.noreply.github.com>
2024-09-12 23:57:34 -0500

    update Component header and start Page

--- app/shopping/page.tsx
+++ app/shopping/page.tsx
@@ -5,7 +5,7 @@
 
 import { useState, useEffect } from "react";
 import Dish from "@/components/Dish";
-import Link from "next/link";
+
 
 
 
@@ -192,11 +192,6 @@ const handleRemoveFromCart = (id: number) => {
 
 return (
   <div className="container mx-auto p-8">
-    <h1 className="text-3xl font-bold mb-4">Restaurant Menu</h1>
-    <Link href="/admin">Admin Dishes</Link>
-    <Link href="/shopping">Shopping Cart</Link>
-
-
     {/* Cart Section */}
     <div className="mt-8 mb-8 p-4 border rounded-md shadow-md">
       <h2 className="text-xl font-bold mb-2">Shopping Cart</h2>
---
+++ components/Header.tsx
@@ -0,0 +1,34 @@
+
+
+import Link from 'next/link';
+import { FC } from 'react';
+
+
+const Header: FC = () => {
+ return (
+   <header className="text-white py-4">
+     <nav className="container mx-auto flex justify-between items-center">
+       <div className="logo text-2xl font-bold">
+         <Link href="/">
+         Restaurant UI
+         </Link>
+       </div>
+       <ul className="flex space-x-6">
+         <li>
+           <Link href="/admin" className="hover:underline">
+             Admin Dishes
+           </Link>
+         </li>
+         <li>
+           <Link href="/shopping" className="hover:underline">
+            Shopping Cart
+           </Link>
+         </li>
+       </ul>
+     </nav>
+   </header>
+ );
+};
+
+
+export default Header;
