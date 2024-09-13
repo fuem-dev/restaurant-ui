c758edf7dbc1bfc12b861294be26ca9dc6949410
fuem-dev <60617260+fuem-dev@users.noreply.github.com>
2024-09-13 05:43:19 -0500

    Update Crud dishes

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,125 +1,213 @@
-'use client'; // Necesario para usar hooks en componentes en el App Router
+
+'use client'; // Necessary for using hooks in components within the App Router
 
 
 import { useState } from 'react';
-import Dish from '../../components/Dish'; // Asegúrate de que la ruta del componente Dish sea correcta
-
-
-// Dishes iniciales para mostrar en el menú
-const dishesList = [
- { id: 1, name: 'Pizza Margarita', description: 'Tomate, mozzarella y albahaca.', difficulty: 'easy', title: 'Pizza', image: 'path_to_image1', category: 'new category' },
- { id: 2, name: 'Pasta Carbonara', description: 'Pasta con salsa carbonara y panceta.', difficulty: 'medium', title: 'Pasta', image: 'path_to_image2', category: 'salad'  },
-];
+import Dish from '../../components/Dish'; // Ensure that the path to the Dish component is correct
+import dishesList from '../static/dishes.json'; // Import the JSON file containing the initial list of dishes
 
 
 export default function Menu() {
- const [dishes, setDishes] = useState(dishesList);
- const [newDish, setNewDish] = useState({ id: 0, name: '', price: 0, description: '', difficulty: '', title: '', image: '', category:'' });
+ const [dishes, setDishes] = useState(dishesList); // State to store the list of dishes
+ const [newDish, setNewDish] = useState({
+   id: 0,
+   name: '',
+   price: 0,
+   description: '',
+   difficulty: '',
+   title: '',
+   image: '',
+   category: ''
+ });
 
 
+ // Function to handle adding a new dish
  const handleSubmit = (e: React.FormEvent) => {
    e.preventDefault();
-   setDishes([...dishes, { ...newDish, id: dishes.length + 1 }]);
-   setNewDish({ id: 0, name: '', price: 0, description: '', difficulty: '', title: '', image: '', category:'' });
+   setDishes([...dishes, { ...newDish, id: dishes.length + 1 }]); // Add the new dish to the list
+   setNewDish({
+     id: 0,
+     name: '',
+     price: 0,
+     description: '',
+     difficulty: '',
+     title: '',
+     image: '',
+     category: ''
+   }); // Reset the new dish form
  };
 
 
+ // Function to handle adding a dish to the cart
  const handleAddToCart = (id: number) => {
    console.log(`Adding dish with ID: ${id} to cart`);
    // Add your logic for adding the dish to the cart
  };
 
 
+ // Function to handle removing a dish from the cart
  const handleRemoveFromCart = (id: number) => {
    console.log(`Removing dish with ID: ${id} from cart`);
    // Add your logic for removing the dish from the cart
  };
 
 
- return (
+ // Function to handle removing a dish from the menu
+ const handleRemoveDish = (id: number) => {
+   const updatedDishes = dishes.filter(dish => dish.id !== id); // Remove the dish from the list
+   setDishes(updatedDishes);
+ };
+
+
+ return ( 
    <div className="container mx-auto p-8">
-     <h1 className="text-3xl font-bold mb-4">Menú del Restaurante</h1>
+     <h1 className="text-3xl font-bold mb-4">Restaurant Menu</h1>
 
 
-     {/* Formulario para agregar un nuevo plato */}
-     <form onSubmit={handleSubmit} className="mb-4">
-       <input
-         type="text"
-         placeholder="Nombre del plato"
-         value={newDish.name}
-         onChange={(e) => setNewDish({ ...newDish, name: e.target.value })}
-         className="block p-2 border border-gray-300 rounded-md mb-2"
-         required
-       />
-       <input
-         type="number"
-         placeholder="Precio"
-         value={newDish.price}
-         onChange={(e) => setNewDish({ ...newDish, price: Number(e.target.value) })}
-         className="block p-2 border border-gray-300 rounded-md mb-2"
-         required
-       />
-       <input
-         type="text"
-         placeholder="Category"
-         value={newDish.category}
-         onChange={(e) => setNewDish({ ...newDish, category: e.target.value })}
-         className="block p-2 border border-gray-300 rounded-md mb-2"
-         required
-       />
-       <input
-         type="text"
-         placeholder="Dificultad"
-         value={newDish.difficulty}
-         onChange={(e) => setNewDish({ ...newDish, difficulty: e.target.value })}
-         className="block p-2 border border-gray-300 rounded-md mb-2"
-         required
-       />
-       <input
-         type="text"
-         placeholder="Título"
-         value={newDish.title}
-         onChange={(e) => setNewDish({ ...newDish, title: e.target.value })}
-         className="block p-2 border border-gray-300 rounded-md mb-2"
-         required
-       />
-       <textarea
-         placeholder="Descripción"
-         value={newDish.description}
-         onChange={(e) => setNewDish({ ...newDish, description: e.target.value })}
-         className="block p-2 border border-gray-300 rounded-md mb-2"
-         required
-       ></textarea>
-       <input
-         type="text"
-         placeholder="URL de la Imagen"
-         value={newDish.image}
-         onChange={(e) => setNewDish({ ...newDish, image: e.target.value })}
-         className="block p-2 border border-gray-300 rounded-md mb-2"
-       />
-       <button type="submit" className="bg-blue-500 text-white p-2 rounded-md">
-         Agregar Plato
+     {/* Form to add a new dish */}
+     <form onSubmit={handleSubmit} className="mb-4 p-4 rounded-lg w-full">
+       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-4 w-full">
+         {/* Dish Name Input */}
+         <div className="flex flex-col w-full">
+           <label htmlFor="name" className="text-sm font-medium text-white">Dish Name</label>
+           <input
+             type="text"
+             id="name"
+             placeholder="Enter the dish name"
+             value={newDish.name}
+             onChange={(e) => setNewDish({ ...newDish, name: e.target.value })}
+             className="p-2 border border-gray-300 rounded-md bg-[#2C2C2C] text-gray-300"
+             required
+           />
+         </div>
+
+
+         {/* Price Input */}
+         <div className="flex flex-col w-full">
+           <label htmlFor="price" className="text-sm font-medium text-white">Price ($)</label>
+           <input
+             type="number"
+             id="price"
+             placeholder="Enter the price"
+             value={newDish.price}
+             onChange={(e) => setNewDish({ ...newDish, price: Number(e.target.value) })}
+             className="p-2 border border-gray-300 rounded-md bg-[#2C2C2C] text-gray-300"
+             required
+           />
+         </div>
+
+
+         {/* Category Input */}
+         <div className="flex flex-col w-full">
+           <label htmlFor="category" className="text-sm font-medium text-white">Category</label>
+           <input
+             type="text"
+             id="category"
+             placeholder="Enter the category"
+             value={newDish.category}
+             onChange={(e) => setNewDish({ ...newDish, category: e.target.value })}
+             className="p-2 border border-gray-300 rounded-md bg-[#2C2C2C] text-gray-300"
+             required
+           />
+         </div>
+
+
+         {/* Difficulty Input */}
+         <div className="flex flex-col w-full">
+           <label htmlFor="difficulty" className="text-sm font-medium text-white">Difficulty Level</label>
+           <input
+             type="text"
+             id="difficulty"
+             placeholder="Enter the difficulty level"
+             value={newDish.difficulty}
+             onChange={(e) => setNewDish({ ...newDish, difficulty: e.target.value })}
+             className="p-2 border border-gray-300 rounded-md bg-[#2C2C2C] text-gray-300"
+             required
+           />
+         </div>
+
+
+         {/* Title Input */}
+         <div className="flex flex-col w-full">
+           <label htmlFor="title" className="text-sm font-medium text-white">Title</label>
+           <input
+             type="text"
+             id="title"
+             placeholder="Enter the title"
+             value={newDish.title}
+             onChange={(e) => setNewDish({ ...newDish, title: e.target.value })}
+             className="p-2 border border-gray-300 rounded-md bg-[#2C2C2C] text-gray-300"
+             required
+           />
+         </div>
+
+
+         {/* Image URL Input */}
+         <div className="flex flex-col w-full">
+           <label htmlFor="image" className="text-sm font-medium text-white">Image URL</label>
+           <input
+             type="text"
+             id="image"
+             placeholder="Enter the image URL"
+             value={newDish.image}
+             onChange={(e) => setNewDish({ ...newDish, image: e.target.value })}
+             className="p-2 border border-gray-300 rounded-md bg-[#2C2C2C] text-gray-300"
+           />
+         </div>
+       </div>
+
+
+       {/* Description Input - Full Width */}
+       <div className="flex flex-col w-full mb-4">
+         <label htmlFor="description" className="text-sm font-medium text-white">Description</label>
+         <textarea
+           id="description"
+           placeholder="Enter a brief description"
+           value={newDish.description}
+           onChange={(e) => setNewDish({ ...newDish, description: e.target.value })}
+           className="p-2 border border-gray-300 rounded-md bg-[#2C2C2C] text-gray-300"
+           required
+         ></textarea>
+       </div>
+
+
+       {/* Submit Button */}
+       <button type="submit" className="bg-gray-700 text-white p-2 rounded-md hover:bg-gray-600 transition-colors">
+         Add Dish
        </button>
      </form>
 
 
-     {/* Listado de platos */}
-     <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-       {dishes.map((dish, index) => (
-         <Dish
-           key={index}
-           id={dish.id}
-           name={dish.name}
-           title={dish.title}
-           description={dish.description}
-           difficulty={dish.difficulty}
-           image={dish.image}
-           onAddToCar={handleAddToCart}
-           onRemoveFromCar={handleRemoveFromCart}
-           hideButtons={true}
-         />
+     {/* List of dishes */}
+     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+       {dishes.map((dish) => (
+         <div key={dish.id} className="relative">
+           <Dish
+             id={dish.id}
+             name={dish.name}
+             title={dish.title}
+             description={dish.description}
+             difficulty={dish.difficulty}
+             image={dish.image}
+             onAddToCar={handleAddToCart}
+             onRemoveFromCar={handleRemoveFromCart}
+             hideButtons={true}
+             displayAsTable={true}
+           />
+
+
+           {/* Remove Dish Button */}
+           <button
+             onClick={() => handleRemoveDish(dish.id)}
+             className="absolute top-4 right-4 bg-white-500 text-white p-1 rounded-full hover:bg-white-600 transition-colors"
+           >
+             X
+           </button>
+         </div>
        ))}
      </div>
    </div>
  );
 }
+
+
--- components/Dish.tsx
+++ components/Dish.tsx
@@ -3,95 +3,84 @@
 import React from 'react';
 
 
-
-
 interface DishProps {
-id: number;
-name: string;
-description: string;
-difficulty: string;
-title?: string;  // 'title' es opcional
-image?: string;  // 'image' es opcional
-onAddToCar: (id: number) => void;  // Función para agregar al carrito
-onRemoveFromCar: (id: number) => void;  // Función para remover del carrito
-hideButtons?: boolean;  // Propiedad para ocultar los botones
-displayAsTable?: boolean;  // Nueva propiedad para mostrar como tabla
+ id: number;
+ name: string;
+ description: string;
+ difficulty: string;
+ title?: string;  // 'title' is optional
+ image?: string;  // 'image' is optional
+ onAddToCar: (id: number) => void;  // Function to add to cart
+ onRemoveFromCar: (id: number) => void;  // Function to remove from cart
+ hideButtons?: boolean;  // Property to hide buttons
+ displayAsTable?: boolean;  // New property to display as table
 }
 
 
+const Dish: React.FC<DishProps> = ({
+ title,
+ id,
+ image,
+ difficulty,
+ onAddToCar,
+ onRemoveFromCar,
+ hideButtons,
+ displayAsTable,
+}) => {
+ // Render as table if displayAsTable is true
+ if (displayAsTable) {
+   return (
+     <article className="p-4 card-bg-css shadow-md rounded-md">
+       <span className="pt-0 pb-0 pl-4 block mt-2 font-semibold text-sm md:text-xs">
+         {/* Font size is small on tablets */}
+         <span className="text-white">Difficulty:</span>
+         <span className="text-green-500"> {difficulty}</span>
+       </span>
+       <figure className="p-4">
+         {image && <img src={image} alt="image description" className="rounded-md" />}
+         {title && <figcaption className="pt-4 font-bold text-sm md:text-xs">{title}</figcaption>}
+       </figure>
+       <p className="text-green-500 pl-4 text-sm md:text-xs">PRICE: {`$ ${id}.00`}</p>
+     </article>
+   );
+ }
 
 
-const Dish: React.FC<DishProps> = ({ title, id, image, difficulty, onAddToCar, onRemoveFromCar, hideButtons, displayAsTable }) => {
-// Renderizar como tabla si displayAsTable es true
-if (displayAsTable) {
-  return (
-    <tr>
-      <td className="p-4">{id}</td>
-      <td className="p-4">{title}</td>
-      <td className="p-4">{difficulty}</td>
-      <td className="p-4">
-        {!hideButtons && (
-          <div className="flex space-x-4">
-            <button
-              className="bg-red-500 text-white px-4 py-2 rounded-md"
-              onClick={() => onRemoveFromCar(id)}
-            >
-              -
-            </button>
-            <button
-              className="bg-green-500 text-white px-4 py-2 rounded-md"
-              onClick={() => onAddToCar(id)}
-            >
-              +
-            </button>
-          </div>
-        )}
-      </td>
-    </tr>
-  );
-}
+ // Render as card if displayAsTable is false or not present
+ return (
+   <article className="p-4 card-bg-css shadow-md rounded-md">
+     <span className="pt-0 pb-0 pl-4 block mt-2 font-semibold text-sm md:text-xs">
+       {/* Font size is small on tablets */}
+       <span className="text-white">Difficulty:</span>
+       <span className="text-green-500"> {difficulty}</span>
+     </span>
+     <figure className="p-4">
+       {image && <img src={image} alt="image description" className="rounded-md" />}
+       {title && <figcaption className="pt-4 font-bold text-sm md:text-xs">{title}</figcaption>}
+     </figure>
+     <p className="text-green-500 pl-4 text-sm md:text-xs">PRICE: {`$ ${id}.00`}</p>
 
 
-
-
-// Renderizar como tarjeta si displayAsTable es false o no está presente
-return (
-  <article className="p-4 card-bg-css shadow-md rounded-md">
-    <span className="pt-0 pb-0 pl-4 block mt-2 font-semibold">
-      <span className="text-white">Difficulty:</span>
-      <span className="text-green-500"> {difficulty}</span>
-    </span>
-    <figure className="p-4">
-      {image && <img src={image} alt="image description" className='rounded-md' />}
-      {title && <figcaption className='pt-4 font-bold'>{title}</figcaption>}
-    </figure>
-    <p className="text-green-500 pl-4">PRICE: {`$ ${id}.00`}</p>
-
-
-
-
-    {/* Botones para agregar y remover, se ocultan si hideButtons es true */}
-    {!hideButtons && (
-      <div className="flex space-x-4 mt-4 justify-end pr-4">
-        <button
-          className="bg-red-500 text-white px-4 py-2 rounded-md"
-          onClick={() => onRemoveFromCar(id)}
-        >
-          -
-        </button>
-        <button
-          className="bg-green-500 text-white px-4 py-2 rounded-md"
-          onClick={() => onAddToCar(id)}
-        >
-          +
-        </button>
-      </div>
-    )}
-  </article>
-);
+     {/* Buttons to add and remove, hidden if hideButtons is true */}
+     {!hideButtons && (
+       <div className="flex space-x-4 mt-4 justify-end pr-4">
+         <button
+           className="bg-red-500 text-white px-4 py-2 rounded-md"
+           onClick={() => onRemoveFromCar(id)}
+         >
+           -
+         </button>
+         <button
+           className="bg-green-500 text-white px-4 py-2 rounded-md"
+           onClick={() => onAddToCar(id)}
+         >
+           +
+         </button>
+       </div>
+     )}
+   </article>
+ );
 };
-
-
 
 
 export default Dish;
