489ef67e33c03cabf3ae9ac1c17ed86ea729655b
fuem-dev <60617260+fuem-dev@users.noreply.github.com>
2024-09-13 01:31:02 -0500

    start Page styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-"use client"; // Indicates that this is a Client Component
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="container mx-auto p-8">
-      <h1>Start page</h1>
+    <div className="flex justify-center items-center min-h-screen overflow-hidden -mt-20 ">
+      <div className="inline-block rounded-lg text-center">
+        <img src="/mainImage.png" alt="Descripción de la imagen" className="rounded-lg -p-10px" />
+        {/* Botón verde debajo de la imagen utilizando Link sin el tag <a> */}
+        <Link href="/shopping" className="mt-4 px-20 py-4 text-2xl font-bold bg-green-500 text-white rounded-full hover:bg-green-700">
+            Get started
+        </Link>
+      </div>
     </div>
   );
 }
